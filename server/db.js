e8c470ee022a588519afc491bbc200c5e69a860e
Dmitry Rybakov <40833223+bulletmys@users.noreply.github.com>
2018-07-11 03:03:47 +0300

    Создана отрисовка сообщений, дополнено переключение
    Дополнено переключение между диалогами, создана отрисовка сообщений в чате, добавлена кнопка отправки сообщений.

---
+++ server/db.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const mongoose = require('./modules/db');
+
+mongoose.connection.on('open', () => {
+    const db = mongoose.connection.db;
+
+    db.dropDatabase((error) => {
+        if (error) throw error;
+
+        // TODO
+
+        mongoose.disconnect();
+    });
+});
