2649196eeb93ad6a9e324015170afd64120fe613
Arsen Gavrilenko <senya.gavrilenko@gmail.com>
2018-07-14 11:16:44 +0300

    Исправлены отступы

--- server/app.js
+++ server/app.js
@@ -1,14 +1,14 @@
 const http = require('http');
 const Router = require('./modules/router');
 
-var port = 8080;
+const port = 8080;
 
 const app = new Router(`http://localhost:${port}`);
 
 const server = http.createServer((request, response) => {
-  app.routing(request, response);
+    app.routing(request, response);
 });
 
 server.listen(port, 'localhost', () => {
-  console.log(`Сервер запущен: http://localhost:${port}`);
+    console.log(`Сервер запущен: http://localhost:${port}`);
 });
