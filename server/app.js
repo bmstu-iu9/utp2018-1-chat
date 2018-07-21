d7018433e8a0593579f96da30de925a977e6b28c
Arsen Gavrilenko <senya.gavrilenko@gmail.com>
2018-07-21 14:49:35 +0300

    [FIX] Роутинг

--- server/app.js
+++ server/app.js
@@ -2,14 +2,14 @@
 require('app-module-path').addPath(__dirname + '/modules');
 
 const http = require('http');
-const Router = require('/router');
+const Router = require('router');
 
 const port = 8080;
 
 const router = new Router(`http://localhost:${port}`);
 
 const server = http.createServer((request, response) => {
-    app.routing(request, response);
+    router.routing(request, response);
 });
 
 server.listen(port, 'localhost', () => {
