615439ffd69f8677d7a3bec208878a15ceb218b8
Arsen Gavrilenko <senya.gavrilenko@gmail.com>
2018-07-29 22:27:48 +0300

    Скрипт отправки сообщений

--- client/js/common.js
+++ client/js/common.js
@@ -30,7 +30,6 @@ document.addEventListener('DOMContentLoaded', function() {
         if (ev.target.value.trim() === '')
             clsHelper.remove(ev.target.parentNode, 'input_fill');
     }
-    /* === END Обработка input на странице авторизации === */
 }, false);
 
 /* === Для работы с классами === */
@@ -96,70 +95,6 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
 })(window);
-/* === END Для работы с классами === */
-
-/* === Переключение между диалогами === */
-function switchDialog(evt, dlg) {
-    let tabContent = document.getElementsByClassName('tab-content');
-    for (let i = 0; i < tabContent.length; i++)
-        tabContent[i].style.display = 'none';
-
-    let tabLinks = document.getElementsByClassName('tab-links');
-    for (let i = 0; i < tabLinks.length; i++)
-        tabLinks[i].className = tabLinks[i].className.replace(' focus', '');
-
-    document.getElementById(dlg).style.display = 'block';
-    evt.currentTarget.className += ' focus';
-
-    document.forms['messageField'].elements['message'].value = '';
-
-    console.log('Переключено на ', evt.currentTarget);
-}
-/* === END Переключение между диалогами === */
-
-/* === Отрисовка сообщений == */
-function sendMessage() {
-
-    //Поиск выбранного диалога
-    let tabContent = document.getElementsByClassName('tab-content');
-    let num = 0;
-    for (let i = 0; i < tabContent.length; i++) {
-        if (tabContent[i].style.display === 'block') {
-            num = i + 1;
-            break;
-        }
-    }
-
-    //Если пользователь не переключал диалоги, то нужно выбрать тот, который загружается по умолчанию
-    if (num === 0)
-        num = 1;
-
-    //Составление HTML кода для вставки
-    const MSG = document.forms['messageField'].elements['message'].value;
-    const newLi = document.createElement('li');
-    newLi.className = 'sent';
-    newLi.innerHTML = '<div class="msg"><div class="msg-header">'
-        + new Date()
-        + '</div><div class="msg-content text">'
-        + MSG
-        + '</div></div><div class="profile"><img src="img/user6.jpg"></div>';
-
-    //Вставка HTML кода
-    let test = document
-        .getElementById(num.toString())
-        .getElementsByClassName('chat-messages')
-        .item(0)
-        .appendChild(newLi);
-
-    // Опускаем scrollbar вниз
-    let chatScroll = document.getElementById(num.toString())
-                            .querySelector('.chat-messages');
-
-    chatScroll.scrollTop = chatScroll.scrollHeight - chatScroll.clientHeight;
-
-    console.log('Добавлено сообщение ', test);
-}
-/* === END Отрисовка сообщений == */
 
 /* === Авторизация === */
 function auth(event) {
@@ -169,7 +104,6 @@ function auth(event) {
         document.getElementById('signin-submit').click();
     }
 }
-/* === END Авторищация */
 
 /* === Преключение формы === */
 function flip() {
@@ -181,4 +115,3 @@ function flip() {
         clsHelper.add(document.getElementById('auth-flip'), 'flip-on')
     }
 }
-/* === END Преключение формы === */
