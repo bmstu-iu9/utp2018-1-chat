108da3ed3a9a489923e13c9203cb2786aa178bad
Arsen Gavrilenko <senya-g@protonmail.com>
2018-08-14 11:58:24 +0300

    Обновлены стили и скрипты
    Co-Authored-By: volhamster <volhamster@users.noreply.github.com>

--- client/js/common.js
+++ client/js/common.js
@@ -16,85 +16,22 @@ document.addEventListener('DOMContentLoaded', function () {
 
     [].slice.call(document.querySelectorAll('input.input-field')).forEach(function (inputEl) {
         if (inputEl.value.trim() !== '')
-            clsHelper.add(inputEl.parentNode, 'input_fill');
+            cssClasses.add(inputEl.parentNode, 'input_fill');
 
         inputEl.addEventListener('focus', onInputFocus);
         inputEl.addEventListener('blur', onInputBlur);
     });
 
     function onInputFocus(ev) {
-        clsHelper.add(ev.target.parentNode, 'input_fill');
+        cssClasses.add(ev.target.parentNode, 'input_fill');
     }
 
     function onInputBlur(ev) {
         if (ev.target.value.trim() === '')
-            clsHelper.remove(ev.target.parentNode, 'input_fill');
+            cssClasses.remove(ev.target.parentNode, 'input_fill');
     }
 }, false);
 
-/* === Для работы с классами === */
-((window) => {
-    function classReg(className) {
-        return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
-    }
-
-    let addClass;
-    let removeClass;
-    let hasClass;
-
-    if ('classList' in document.documentElement) {
-        hasClass = function (el, cls) {
-            return el.classList.contains(cls);
-        };
-
-        addClass = function (el, cls) {
-            el.classList.add(cls);
-        };
-
-        removeClass = function (el, cls) {
-            el.classList.remove(cls);
-        };
-    } else {
-        hasClass = function (el, cls) {
-            return classReg(cls).test(el.className);
-        };
-
-        addClass = function (el, cls) {
-            if (!hasClass(el, cls)) {
-                el.className = el.className + ' ' + cls;
-            }
-        };
-
-        removeClass = function (el, cls) {
-            el.className = el.className.replace(classReg(cls), ' ');
-        };
-    }
-
-    function toggleClass(el, cls) {
-        let func;
-
-        if (hasClass(el, cls))
-            func = removeClass;
-        else
-            func = addClass;
-
-        func(el, cls);
-    }
-
-    let clsHelper = {
-        has: hasClass,
-        add: addClass,
-        remove: removeClass,
-        toggle: toggleClass
-    };
-
-    if (typeof define === 'function' && define.amd) {
-        define(clsHelper);
-    } else {
-        window.clsHelper = clsHelper;
-    }
-
-})(window);
 
 /* === Смена стиля == */
 function changeStyle() {
@@ -131,9 +68,9 @@ function auth(event) {
 function flip() {
     const flipper = document.getElementById('auth-flip');
 
-    if (clsHelper.has(flipper, 'flip-on')) {
-        clsHelper.remove(document.getElementById('auth-flip'), 'flip-on')
+    if (cssClasses.has(flipper, 'flip-on')) {
+        cssClasses.remove(document.getElementById('auth-flip'), 'flip-on')
     } else {
-        clsHelper.add(document.getElementById('auth-flip'), 'flip-on')
+        cssClasses.add(document.getElementById('auth-flip'), 'flip-on')
     }
 }
