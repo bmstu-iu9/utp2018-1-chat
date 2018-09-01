8d95b6dc12b17aa5b171167462de71b3c322b479
Tataklim <tatakaklim@gmail.com>
2018-09-01 17:34:32 +0300

    Дополнения к логам типа Info

--- application/modules/log/index.js
+++ application/modules/log/index.js
@@ -7,7 +7,7 @@ const Database = require('db');
 const config = require('log/config');
 
 module.exports.format = async (level, date, message) => {
-    return [level, ' [', date, '] ', message].join('');
+    return [level, ' [', date, '] : ', message].join('');
 }
 
 module.exports.info = async (msg) => {
@@ -23,6 +23,44 @@ module.exports.info = async (msg) => {
         //Запись в БД
     }
 };
+
+module.exports.format = async (level, date, prefix, message) => {
+    return [level, ' [', date, '] ', prefix.toUpperCase(), message].join('');
+}
+
+module.exports.info = async (msg, prefix, col) => {
+    if (config['method'] == 'console'){
+        let datee = new Date().toUTCString();
+        let textt = '[Info]' + ' [' + datee + '] : ' + prefix.toUpperCase() + ' ' + msg;
+        let a = 0;
+        let splitText = textt.split(' ');
+        let cssRules = [];
+        let styledText = '';
+        for (var split of splitText)  {
+            if (a == 0) {
+                cssRules.push('color:green');
+                a++;
+            } else if (a == 8) {
+                cssRules.push('color:' + col);
+                a++;
+            } else {
+                cssRules.push('color:black')
+                a++;
+            }
+            styledText += `%c${split} `
+        };
+        console.log(styledText , ...cssRules)
+
+    } else if (config['method'] == 'file'){
+        fs.writeFile(config['wayForFile', '/n'];
+        fs.writeFile(config['wayForFile'], await format('Info', new Date().toUTCString(), prefix ,msg), function(error){
+                if(error) throw error;
+});
+    } else {
+        //Запись в БД
+    }
+};
+
 
 module.exports.error = async (msg) => {
     if (config['method'] == 'console'){
